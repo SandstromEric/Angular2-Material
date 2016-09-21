d672f2229d65bd05ca8505b5e264c9aa4face4ed
Eric Sandström <Eric Sandstrm>
2016-09-21 04:09:21 +0200

    Created project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdCardModule } from '@angular2-material/card';
+import { MdProgressCircleModule } from '@angular2-material/progress-circle';
+import { MdSidenavModule } from '@angular2-material/sidenav';
+import { MdInputModule } from '@angular2-material/input';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdButtonModule.forRoot(),
+    MdCardModule.forRoot(),
+    MdProgressCircleModule.forRoot(),
+    MdSidenavModule.forRoot(),
+    MdInputModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
